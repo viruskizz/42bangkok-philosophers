4a069d637cd89ec49b9fc9f25717b1df0b2fbec8
araiva <kizzaraiva@gmail.com>
2022-10-03 18:36:38 +0700

    rm libft

--- includes/philosophers.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
-
-# include "libft.h"
-# include <stdbool.h>  
-# include <stdio.h>
-# include <unistd.h>
-
-#endif
--- srcs/main.c
+++
@@ -1,6 +0,0 @@
-
-int	main(int argc, char *argv[])
-{
-	printf("Test\n");
-	return (0);
-}
